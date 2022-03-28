7bff76238591dfdf50a5920c9dcc46ed8cc1b254
Yngvine <74989667+Yngvine@users.noreply.github.com>
2022-03-28 12:36:20 +0200

    Se añade ejemplo de prueba a creaTrazaTXT.c

--- creaTrazaTXT.c
+++ creaTrazaTXT.c
@@ -5,10 +5,12 @@
 
 void ex1();
 void ex2();
+void ex3();
 
 int main(void) {
     //ex1();
-    ex2();
+    //ex2();
+    ex3();
 
     return(0);
 }
@@ -29,6 +31,19 @@ void ex2() {
     f = fopen(FILENAME,"w");
     unsigned long dirBase = 0x0000;
     int columnas = 1024, filas =4;
+
+    for (int j=0; j<columnas; j++)
+        for ( int i=0; i<filas; i++){
+            fprintf(f, "0x%lX ", dirBase+j*8+columnas*i*8);
+        }
+    fclose(f);
+}
+
+void ex3() {
+    FILE *f;
+    f = fopen(FILENAME,"w");
+    unsigned long dirBase = 0x0000;
+    int columnas = 8, filas = 5;
 
     for (int j=0; j<columnas; j++)
         for ( int i=0; i<filas; i++){
